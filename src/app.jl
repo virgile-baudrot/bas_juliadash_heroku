c8658f45b63dabae7f6e02e4cd2246dd37f4bae6
vbaudrot <virgile.baudrot@posteo.net>
2021-01-27 16:02:03 +0100

    test Dispersal

--- src/app.jl
+++ src/app.jl
@@ -1,13 +1,40 @@
 using Mux
+using Interact
+using Plots
+using Dispersal
 
+init =  [0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0;
+         0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0;
+         0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0;
+         0.0 0.0 0.0 0.0 81. 0.0 0.0 0.0;
+         0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0;
+         0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0;
+         0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0;
+         0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0]
+
+struct TestFormulation <: KernelFormulation end
+(f::TestFormulation)(d) = 1.0
+
+hood = DispersalKernel{1}(; formulation=TestFormulation())
+ruleset = Ruleset(InwardsDispersal(;neighborhood=hood))
+tspan = 1:1:10
+output = ArrayOutput(init; tspan=tspan, store=true)
+sim!(output, ruleset)
+
+mp = @manipulate for t in slider(tspan; value = minimum(tspan), label="Time")
+    Plots.heatmap(
+        output[t],
+        c = cgrad(:roma),
+        clims=(0.0, 100.0),
+        title="Time $t"
+    )
+end
+
+# WebIO.webio_serve(page("/", req -> mp),8095) # serve on a random port
 @app test = (
-  Mux.defaults,
-  page(respond("<h1>Hello JuliaCon!</h1>")),
-  page("/about",
-       probabilty(0.1, respond("<h1>Boo!</h1>")),
-       respond("<h1>About Me</h1>")),
-  page("/user/:user", req -> "<h1>Hello, $(req[:params][:user])!</h1>"),
-  Mux.notfound())
+    Mux.defaults,
+    Mux.page("/", req -> mp),
+    Mux.notfound())
 
+# serve(test, 8096)
 fetch(serve(test,parse(Int,ARGS[1])))
-
